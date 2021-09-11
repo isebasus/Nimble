4f70bc6c9eb5cf98973eec1c22653014904f0e11
Sebastian <isebasus@gmail.com>
2021-09-11 14:18:09 -0700

    just added some info thing

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -6,17 +6,11 @@ import pongVideo from '../src/videos/pongVideo.mp4';
 import scraperVideo from '../src/videos/scraperVideo.mp4';
 import archiveVideo from '../src/videos/archive.mp4';
 import pacVideo from '../src/videos/pacman.mp4';
-import Project from './components/Projects.jsx';
 import Description from './components/Description.jsx';
-import { ChooseShirt } from './components/ChooseShirt.js';
-import FetchData from './components/FetchData.js'
-import CustomImageEditor from './components/Designer.js';
+import BlankDescription from './components/BlankDescription.jsx';
+import Project from '../src/components/Projects.jsx';
 import { createBrowserHistory } from 'history';
-import qwerty from '../src/images/qwerty.png'
 import './styles/App.css';
-
-var xRotate = 0;
-var yRotate = 0;
 
 function App() {
   return (
@@ -38,12 +32,7 @@ function App() {
   );
 }
 
-class Home extends React.Component{
-  onHover = () =>{
-    xRotate = 1;
-    yRotate = 1;
-  }
-
+class Home extends React.Component {
   qwerty = () => {
     this.props.history.push('/designer')
   }
@@ -72,11 +61,9 @@ class Home extends React.Component{
     this.props.history.push('/foodbanks')
   }
 
-
   ChooseMerch = () => {
     this.props.history.push('/choose-merch')
   }
-
 
   render(){
     return (
@@ -109,9 +96,87 @@ class Home extends React.Component{
 }
 
 export class ChooseMerch extends React.Component {
+
+  static location = Location;
+
+  constructor(props){
+    super(props);
+    this.location = props.location;
+    this.overrideLocation();
+  };
+
+  losAngelesApparel = () => {
+    this.props.history.push(`${this.props.match.path}/la-apparel`);
+  }
+
   render() {
     return (
-      <ChooseShirt /> 
+        <div className="body">
+            <nav>
+                <a href="https://github.com/isebasus/"class="logo" id="g" >
+                    <h1 className="logoText">NIMBLE</h1>
+                </a>
+                <a href="https://github.com/isebasus/"class="github" id="g" >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="40" height="40" fill="rgb(0,0,0)"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+                </a>
+            </nav>
+            <div className="merch">
+                <nav className="navBar">
+                    <a class="navItems">Home</a>
+                    <a class="navItems">About</a>
+                    <a class="navItems">Partners</a>
+                    <a class="navItems">Builder</a>
+                    <a class="navItems">Order</a>
+                </nav>
+
+                <h1 className="first">Choose your Blanks<a id="text"></a></h1>
+                <div className="columns">
+                    <Project projectId="laa" path={this.losAngelesApparel} quality={<a className="quality"> Best Quality</a>} coverId="coverScraper" name="Los Angeles Apparel"></Project>
+                    <Project projectId="gildan" quality={<a className="quality2"> Best Price</a>} path={this.archive} coverId="coverScraper" name="Gildan"></Project>
+                    <Project projectId="comfortColors" path={this.videoScraper} coverId="coverScraper"name="Comfort Colors"></Project>
+                    <Project projectId="alstyle" path={this.videoScraper} coverId="coverScraper"name="Alstyle"></Project>
+                </div>
+            </div>
+            <Switch location={this.location}>
+                <Route exact path={`${this.props.match.path}/`} component={this} />
+            </Switch>
+            <Route path={`${this.props.match.path}/la-apparel`} component={LosAngelesApparel}/>
+        </div>
+    );
+  }
+
+  overrideLocation() {
+    let location = Object.assign({}, this.props.location)
+    location.pathname = this.props.match
+    this.location = location
+  }
+}
+
+export class LosAngelesApparel extends React.Component{
+  constructor(props){
+    super(props);
+  };
+
+  homeClick = () => {
+    this.props.history.push('/choose-merch');
+  }
+
+  render(){
+    return(
+      <BlankDescription 
+      onClick={this.homeClick} 
+      caption="Merch made in the USA." 
+      title="Los Angeles Apparel"
+      padding="130px"
+      imageClass="laaGarments"
+      technology1="devicon-python-plain"
+      technology2="devicon-html5-plain"
+      technology3="devicon-javascript-plain"
+      type1="Python"
+      type2="HTML5/CSS"
+      type3="JavaScript"
+      gitLink="https://github.com/ZumbaMaster313/YoutubeWebScraper"
+      ></BlankDescription>
     )
   }
 }
