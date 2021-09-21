56d1a191b67c69742f36b021b0e483c4a829e5ee
Sebastian <isebasus@gmail.com>
2021-09-21 10:27:03 -0700

    adding los angeles apparel onto this

--- ClientApp/src/components/ShoppingCart.js
+++ ClientApp/src/components/ShoppingCart.js
@@ -12,13 +12,18 @@ export default class ShoppingCart extends Component {
     
     render() {
         return (
-            <div className="shoppingCart">
-              <h2 className="cartTitle">BASKET</h2>
-              <CartItem type="5041GD SHIRT" ammount="24"/>
-              <CartItem type="5041GD SHIRT" ammount="24"/>
-              <CartItem type="5041GD SHIRT" ammount="24"/>
-              <CartItem type="5041GD SHIRT" ammount="24"/>
-              <CartItem type="5041GD SHIRT" ammount="24"/>
+            <div className="shoppingMargin">
+                <div className="expander">
+                    <div className="shoppingCart">
+                        <h2 className="cartTitle">BASKET</h2>
+                        <a></a>
+                        <CartItem type="5041GD SHIRT" ammount="24"/>
+                        <CartItem type="5041GD SHIRT" ammount="24"/>
+                        <CartItem type="5041GD SHIRT" ammount="24"/>
+                        <CartItem type="5041GD SHIRT" ammount="24"/>
+                        <CartItem type="5041GD SHIRT" ammount="24"/>
+                    </div>
+                </div>
             </div>
         );
     }
