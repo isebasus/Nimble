cf5c92a751c0a04ebceec122393da2372dea3eea
Sebastian <isebasus@gmail.com>
2021-12-20 10:32:58 -0800

    added some features

--- NimbleClient/src/components/Mockup.js
+++ NimbleClient/src/components/Mockup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import CartItem from './CartItem.js';
 
 export default class Mockup extends Component {
     constructor(props) {
@@ -31,6 +32,13 @@ export default class Mockup extends Component {
     }
 
     render() {
+        var display;
+        if (this.state.cart.length == 0) {
+            display = <h2 className="caption" >Please add items to your cart.</h2>
+        } else {
+            display = <Items></Items>
+        }
+
         return (
             <div className="body">
               <Header history={this.props.history}></Header>
@@ -42,8 +50,49 @@ export default class Mockup extends Component {
                       <a class="navItems">Builder</a>
                       <a class="navItems">Order</a>
                 </nav>
+                {display}
               </div>
         </div>
         )
     }
 }
+
+export class Items extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = JSON.parse(window.localStorage.getItem('state')) || {
+            cart: []
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            if (window.localStorage.getItem('state') == null) {
+                return;
+            }
+
+            if (JSON.parse(window.localStorage.getItem('state')).cart.length > 0) {
+                this.setState(JSON.parse(window.localStorage.getItem('state')));
+            } else {
+                this.setState({cart: []});
+            }
+        }, 500)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render(){
+      return (
+        <div>
+                <div class="basketItems" style={{gridTemplateColumns: "repeat(1, 1fr)", gap: "15px 15px", overflow: "hidden", opacity: "1", right: "0px", position: "relative", width: "100%"}}>
+                  {this.state.cart.map((item, i) => 
+                    <CartItem name={item.name} color={item.color} units={item.quantity} size={item.size}></CartItem>
+                  )}
+                </div>
+            </div>
+      )
+    }
+  }
